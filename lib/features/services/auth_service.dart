e3f2cc401f23eb4fb3815e89687b7b9ee712f4a2
MugheesMb <mr-mb@tuta.io>
2022-12-21 12:26:11 -0800

    asd

--- lib/features/services/auth_service.dart
+++ lib/features/services/auth_service.dart
@@ -5,7 +5,7 @@ import 'package:last/utils/errorhandle.dart';
 import 'package:last/utils/snak.dart';
 
 
-String uri = 'http://192.168.100.221:3000';
+String uri = 'http://localhost:3000';
 
 class AuthService {
 
@@ -44,4 +44,38 @@ class AuthService {
       showSnackbar(context, e.toString());
     }
   }
+
+  void signInUser({
+    required BuildContext context,
+    required String email,
+    required String password,
+  }) async {
+    try {
+      http.Response res = await http.post(
+        Uri.parse('$uri/api/signin'),
+        body: jsonEncode({'email': email, 'password': password}),
+        headers: <String, String>{
+          'Content-Type': 'application/json; charset=UTF-8',
+        },
+      );
+      httpErrorHandle(
+        response: res,
+        context: context,
+        onSuccess: () async {
+          SharedPreferences prefs = await SharedPreferences.getInstance();
+          Provider.of<UserProvider>(context, listen: false).setUser(res.body);
+          await prefs.setString('x-auth-token', jsonDecode(res.body)['token']);
+          Navigator.pushAndRemoveUntil(
+              context,
+              MaterialPageRoute(
+                  builder: (BuildContext context) => HomeScreen()),
+              ModalRoute.withName('/'));
+        },
+      );
+    } catch (e) {
+      showSnackbar(context, e.toString());
+    }
+  }
+
+
 }
